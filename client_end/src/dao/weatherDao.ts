fcda1c845538f55cb568824a825ba041597a26d1
王琮琅 <11335545+wang-conglang@user.noreply.gitee.com>
2025-03-28 12:03:04 +1300

    deployed DAO structure in front-end

---
+++ client_end/src/dao/weatherDao.ts
@@ -0,0 +1,66 @@
+// src/dao/weatherDao.ts
+
+// 1. Define the weather Data
+// TODO: modify this after negotiate with James
+interface WeatherData {
+  city: string;
+  temp: string;
+  condition: string;
+  high: string;
+  low: string;
+}
+
+// 2. Mocked local data (to replace the real API)
+// TODO: modify this after negotiate with James
+const mockData = {
+  favorites: [
+    {
+      city: "Auckland",
+      temp: "20°",
+      condition: "Sunny",
+      high: "23°",
+      low: "15°",
+    },
+    {
+      city: "Shanghai",
+      temp: "16°",
+      condition: "Clear",
+      high: "28°",
+      low: "16°",
+    },
+  ],
+  hotCities: [
+    {
+      city: "New York",
+      temp: "9°",
+      condition: "Cloudy",
+      high: "9°",
+      low: "1°",
+    },
+    {
+      city: "Paris",
+      temp: "9°",
+      condition: "Partly Cloudy",
+      high: "13°",
+      low: "7°",
+    },
+    // other hot cities etc...
+  ],
+};
+
+// 3. Achieve Dao method
+export default {
+  // Get favorite cities' weather
+  getFavorites(): Promise<WeatherData[]> {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(mockData.favorites), 500); // 模拟网络延迟
+    });
+  },
+
+  // Get hot cities' weather
+  getHotCities(): Promise<WeatherData[]> {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(mockData.hotCities), 500);
+    });
+  },
+};
