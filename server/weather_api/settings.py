a3c4b09b624fc2bfad49f683d7cb504c7fb49077
jcen <cenchao@live.cn>
2025-04-30 10:46:12 +1200

    feat: #22 Fix CROSS issue

--- server/weather_api/settings.py
+++ server/weather_api/settings.py
@@ -1,4 +1,20 @@
 INSTALLED_APPS = [
+    'corsheaders',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     'rest_framework',
     'api',
 ]
+MIDDLEWARE = [
+'corsheaders.middleware.CorsMiddleware',
+'django.middleware.security.SecurityMiddleware',
+'django.contrib.sessions.middleware.SessionMiddleware',
+'django.middleware.common.CommonMiddleware'
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
