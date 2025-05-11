91ccbffc38a077e50356bc6c2d6e7afbbf34bd28
jcen <cenchao@live.cn>
2025-05-11 23:34:29 +1200

    Fix the case issue in routing urls

--- server/weather_api/weather_api/urls.py
+++ server/weather_api/weather_api/urls.py
@@ -2,10 +2,10 @@ from django.urls import re_path
 from api.views import StatusView, SearchCityByNameView, GetCurrentWeatherByCityView, GetForecastWeatherByCityView, RegisterView, LoginView
 
 urlpatterns = [
-    re_path(r'^getStatus/?$', StatusView.as_view(), name='get-status'),
+    re_path(r'^GetStatus/?$', StatusView.as_view(), name='get-status'),
     re_path(r'^SearchCityByName/?$', SearchCityByNameView.as_view(), name='search-city-by-name'),
     re_path(r'^GetCurrentWeatherByCity/?$', GetCurrentWeatherByCityView.as_view(), name='get-current-weather-by-city'),
     re_path(r'^GetForecastWeatherByCity/?$', GetForecastWeatherByCityView.as_view(), name='get-forecast-weather-by-city'),
-    re_path(r'^register/?$', RegisterView.as_view(), name='register'),
-    re_path(r'^login/?$', LoginView.as_view(), name='login'),
+    re_path(r'^Register/?$', RegisterView.as_view(), name='register'),
+    re_path(r'^Login/?$', LoginView.as_view(), name='login'),
 ]
