62ad6c8234c5b7dc537d5f53b4db04d4db91e6f1
王琮琅 <11335545+wang-conglang@user.noreply.gitee.com>
2025-05-26 20:05:24 +1200

    userdao modify

--- client_end/src/dao/userDao.ts
+++ client_end/src/dao/userDao.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export function creatAPI() {
   const api = axios.create({
-    baseURL: "/",
+    baseURL: process.env.VUE_APP_API_URL,
     withCredentials: true,
   });
   return api;
