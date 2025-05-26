601375ef23a57647e63985618685e52c7474f099
王琮琅 <11335545+wang-conglang@user.noreply.gitee.com>
2025-05-26 17:55:22 +1200

    disk test

--- client_end/vue.config.js
+++ client_end/vue.config.js
@@ -4,43 +4,43 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       "/GetStatus": {
-        target: "http://localhost:8080",
+        target: "http://3.104.76.167:8080",
         changeOrigin: true,
       },
       "/SearchCityByName": {
-        target: "http://localhost:8080",
+        target: "http://3.104.76.167:8080",
         changeOrigin: true,
       },
       "/GetCurrentWeatherByCity": {
-        target: "http://localhost:8080",
+        target: "http://3.104.76.167:8080",
         changeOrigin: true,
       },
       "/GetForecastWeatherByCity": {
-        target: "http://localhost:8080",
+        target: "http://3.104.76.167:8080",
         changeOrigin: true,
       },
       "/Login": {
-        target: "http://localhost:8080",
+        target: "http://3.104.76.167:8080",
         changeOrigin: true,
       },
       "/Register": {
-        target: "http://localhost:8080",
+        target: "http://3.104.76.167:8080",
         changeOrigin: true,
       },
       "/GetUserProfile": {
-        target: "http://localhost:8080",
+        target: "http://3.104.76.167:8080",
         changeOrigin: true,
       },
       "/Logout": {
-        target: "http://localhost:8080",
+        target: "http://3.104.76.167:8080",
         changeOrigin: true,
       },
       "/AddFavoriteCity": {
-        target: "http://localhost:8080",
+        target: "http://3.104.76.167:8080",
         changeOrigin: true,
       },
       "/DelFavoriteCity": {
-        target: "http://localhost:8080",
+        target: "http://3.104.76.167:8080",
         changeOrigin: true,
       },
     },
